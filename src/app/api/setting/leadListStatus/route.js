f530768c8dedd30d2258eae699f91c050bacb5b9
bstteam106 <bstteam106@gmail.com>
2026-01-01 19:23:56 +0530

    01/01/2025-1

--- src/app/api/setting/leadListStatus/route.js
+++ src/app/api/setting/leadListStatus/route.js
@@ -234,7 +234,6 @@ export async function GET() {
         }
       }
     ]);
-    console.log(cards);
     return NextResponse.json({ cards }, { status: 201 })
   } catch (error) {
     console.error("Aggregation Error:", error);
